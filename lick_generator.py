488848df64284cc6239b11c487c58584915dbb76
michael-grace <michael.dp.grace@outlook.com>
2021-02-28 07:24:22 +0000

    Notes and Frequencies

--- lick_generator.py
+++ lick_generator.py
@@ -1,6 +1,9 @@
 import wave
 import numpy
 import pygame
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 def lick_maths(start_freq, tempo, ornament, swing):
     calc_tempo = tempo / 30
@@ -26,25 +29,57 @@ def lick_maths(start_freq, tempo, ornament, swing):
 
     return [note for note in notes if note]
 
-sfile = wave.open('lick.wav', 'w')
-sfile.setframerate(48000)
-sfile.setnchannels(2)
-sfile.setsampwidth(2)
-pygame.init()
-pygame.mixer.init(frequency=48000, channels=2)
+@app.route("/")
+def index_handler():
+    # This shows the fun of naming notes!
+    data = {
+        "notes": [
+            (f"{letter[0]}{number}",
+            440 * (2 ** ((number-5) if letter[1]>=3 else (number-4))) * ((2 ** (letter[1]/12)) if letter[1] != 0 else 1)
+            ) 
+        for number in range(9) 
+        for letter in [
+            ("C", 3),
+            ("C#/D♭", 4),
+            ("D", 5),
+            ("D#/E♭", 6),
+            ("E", 7),
+            ("F", 8),
+            ("F#/G♭", 9),
+            ("G", 10),
+            ("G#/A♭", 11),
+            ("A", 0),
+            ("A#/B♭", 1),
+            ("B", 2)
+            ]
+        ]
+    }
+    return render_template("index.html", data=data)
 
-for note in lick_maths(500, 160, None, True):
-    ncycles = note[1] * note[0]
-    spc = int((48000 * note[1]) / ncycles)
+def generate_handler():
+    sfile = wave.open('lick.wav', 'w')
+    sfile.setframerate(48000)
+    sfile.setnchannels(2)
+    sfile.setsampwidth(2)
+    pygame.init()
+    pygame.mixer.init(frequency=48000, channels=2)
 
-    sine = numpy.arange(0, 2*numpy.pi, (2*numpy.pi)/spc)
-    sine = numpy.sin(sine)
-    sine *= 8000
-    sine = numpy.hstack(int(ncycles)*list(sine))
-    sine = numpy.repeat(sine, 2, axis=0)
-    sine.reshape(len(sine)//2,2)
+    for note in lick_maths(500, 160, None, True):
+        ncycles = note[1] * note[0]
+        spc = int((48000 * note[1]) / ncycles)
 
-    snd = pygame.mixer.Sound(sine.astype('int16'))
-    sfile.writeframesraw(snd.get_raw())
+        sine = numpy.arange(0, 2*numpy.pi, (2*numpy.pi)/spc)
+        sine = numpy.sin(sine)
+        sine *= 8000
+        sine = numpy.hstack(int(ncycles)*list(sine))
+        sine = numpy.repeat(sine, 2, axis=0)
+        sine.reshape(len(sine)//2,2)
 
-sfile.close()
+        snd = pygame.mixer.Sound(sine.astype('int16'))
+        sfile.writeframesraw(snd.get_raw())
+
+    sfile.close()
+
+if __name__ == "__main__":
+    
+    app.run()
